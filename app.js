de92b80461b7e359e0671d29160e0ebb15a6c570
Jade Pineda <jadecapy@gmail.com>
2025-03-13 18:53:43 -0500

    3rd time merging and going back to module 1 lol

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('hbs'); // Added Handlebars module
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
+var travelRouter = require('./app_server/routes/travel');
 
 var app = express();
 
 //jade pineda
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// View engine setup
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
+
+// Register Handlebars partials
+hbs.registerPartials(__dirname + '/app_server/views/partials');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,19 +28,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/travel', travelRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
+  
   res.status(err.status || 500);
   res.render('error');
 });
