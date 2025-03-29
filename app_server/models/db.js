79abf4d4daee6b4c7dfc46fa1f3abad80163223d
Jade Pineda <jadecapy@gmail.com>
2025-03-29 17:11:15 -0500

    Add models folder

---
+++ app_server/models/db.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const host = process.env.DB_HOST || '127.0.0.1';
+const dbURI = `mongodb://${host}/travlr`;
+const readLine = require('readline');
+
+// Build the connection string and set a short delay before connecting
+const connect = () => {
+    setTimeout(() => mongoose.connect(dbURI, {}), 1000);
+};
+
+// Connection events
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${dbURI}`);
+});
+
+mongoose.connection.on('error', err => {
+    console.log('Mongoose connection error: ', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+});
+
+// Windows fix for Ctrl+C in terminal
+if (process.platform === 'win32') {
+    const r1 = readLine.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    r1.on('SIGINT', () => {
+        process.emit("SIGINT");
+    });
+}
+
+// Graceful shutdown function
+const gracefulShutdown = (msg) => {
+    mongoose.connection.close(() => {
+        console.log(`Mongoose disconnected through ${msg}`);
+    });
+};
+
+// Shutdown signals
+process.once('SIGUSR2', () => {
+    gracefulShutdown('nodemon restart');
+    process.kill(process.pid, 'SIGUSR2');
+});
+
+process.on('SIGINT', () => {
+    gracefulShutdown('app termination');
+    process.exit(0);
+});
+
+process.on('SIGTERM', () => {
+    gracefulShutdown('app shutdown');
+    process.exit(0);
+});
+
+// Connect to the DB
+connect();
+
+// Import the schema
+require('./travlr');
+
+module.exports = mongoose;
