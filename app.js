b671d59fb8025ddf3af938c5fc7c9c4c9195adad
Jade Pineda <jadecapy@gmail.com>
2025-03-29 17:11:49 -0500

    Organiszed app.js

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-require('./app_server/models/db');
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-// Instead of const exphbs = require('express-handlebars');
-// we now destructure the 'engine' function:
+
 const { engine } = require('express-handlebars');
+
 
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel');
 
+
+require('./app_server/models/db');
+
 const app = express();
 
-// Configure Handlebars
+
 app.engine('hbs', engine({
   extname: 'hbs',
   defaultLayout: 'main',
@@ -25,25 +27,28 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
 
-// catch 404
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
   res.status(err.status || 500);
   res.render('error');
 });
