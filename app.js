befdafa261cbbffb2d78337db73364da5b19c09c
Jade Pineda <jadecapy@gmail.com>
2025-04-05 04:33:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,20 +4,22 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-
 const { engine } = require('express-handlebars');
 
-
+// Import standard routers
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel');
 
+// Import the API router
+const apiRouter = require('./app_api/routes/index');
 
+// Connect to MongoDB using new path
 require('./app_api/models/db');
 
 const app = express();
 
-
+// View engine setup
 app.engine('hbs', engine({
   extname: 'hbs',
   defaultLayout: 'main',
@@ -27,24 +29,25 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 
-
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+// Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter); // 🔥 NEW: All API routes go under /api
 
-
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-
+// Error handler
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
