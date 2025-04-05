94fcba83b753d40d0c5306d4a194c39f601151f7
Jade Pineda <jadecapy@gmail.com>
2025-04-05 04:37:41 -0500

    fix index.js

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express');         // Express app
+const router = express.Router();            // Router logic
 
+// Import the controller
 const tripsController = require('../controllers/trips');
 
+// Route to get all trips
 router
     .route('/trips')
-    .get(tripsController.tripsList);
+    .get(tripsController.tripsList);          // GET /api/trips
 
+// Route to get a trip by tripCode
 router
     .route('/trips/:tripCode')
-    .get(tripsController.tripsFindByCode);
+    .get(tripsController.tripsFindByCode);    // GET /api/trips/GALR210214 (for example)
 
 module.exports = router;
