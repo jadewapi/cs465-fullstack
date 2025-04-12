e16883934be7122553a8ac8fa93503e2c39a7d41
Jade Pineda <jadecapy@gmail.com>
2025-04-12 03:06:20 -0500

    Edit component complete

--- app_admin/src/app/trip-card/trip-card.component.ts
+++ app_admin/src/app/trip-card/trip-card.component.ts
@@ -1,18 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { Trip } from '../models/trip';
 
 @Component({
   selector: 'app-trip-card',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './trip-card.component.html',
-  styleUrls: ['./trip-card.component.css']
+  styleUrl: './trip-card.component.css'
 })
-export class TripCardComponent {
-  @Input() trip!: Trip;
+export class TripCardComponent implements OnInit {
+  @Input('trip') trip: any;
 
   constructor(private router: Router) {}
 
+  ngOnInit(): void {}
+
   public editTrip(trip: Trip): void {
-    this.router.navigate(['/edit-trip', trip.code]);
+    localStorage.removeItem('tripCode');
+    localStorage.setItem('tripCode', trip.code);
+    this.router.navigate(['edit-trip']);
   }
 }
