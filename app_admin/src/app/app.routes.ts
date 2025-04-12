3cf85004676a1f3c113e7380bc0ab06418ad79c9
Jade Pineda <jadecapy@gmail.com>
2025-04-12 02:07:45 -0500

    checkpoint: add trip route not working

--- app_admin/src/app/app.routes.ts
+++ app_admin/src/app/app.routes.ts
@@ -1,8 +1,9 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
 import { TripListingComponent } from './trip-listing/trip-listing.component';
 import { AddTripComponent } from './add-trip/add-trip.component';
 
 export const routes: Routes = [
-  { path: '', component: TripListingComponent, pathMatch: 'full'},
+  { path: '', component: TripListingComponent },
   { path: 'add-trip', component: AddTripComponent }
 ];
