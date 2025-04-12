c409975a01936c6dacfba46377df062a1db107cc
Jade Pineda <jadecapy@gmail.com>
2025-04-11 23:20:10 -0500

    Set up angular

---
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-listing',
+  imports: [],
+  templateUrl: './trip-listing.component.html',
+  styleUrl: './trip-listing.component.css'
+})
+export class TripListingComponent {
+
+}
